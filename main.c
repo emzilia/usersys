62621dc48d5e71cce4a08dc290cea5eff02e6a7d
emzilia <em@zilia.dev>
2025-04-10 08:56:47 -0400

    compartmentalizing dbus calls

--- main.c
+++ main.c
@@ -9,15 +9,42 @@
 #define DESTINATION "org.freedesktop.systemd1"
 #define PATH        "/org/freedesktop/systemd1"
 #define INTERFACE   "org.freedesktop.systemd1.Manager"
-#define MEMBER      "GetUnitFileState"
 
 static int log_error(int error, const char *message) {
 	fprintf(stderr, "%s: %s\n", message, strerror(-error));
 	return error;
 }
 
-void free_mem() {
+sd_bus_message* list_user_units(sd_bus *bus, sd_bus_error error, sd_bus_message *reply) {
+	int r = sd_bus_call_method(
+		bus, 
+		DESTINATION, 
+		PATH, 
+	        INTERFACE, 
+		"ListUnits", 
+	        &error, 
+		&reply, 
+		"a" 
+	);
+	if (r < 0) log_error(r, "ListUnits call failed");
+	return reply;
+}
 
+sd_bus_message* get_unit_status(sd_bus *bus, sd_bus_error error, sd_bus_message *reply, char *unit) {
+	int r = sd_bus_call_method(
+		bus, 
+		DESTINATION, 
+		PATH, 
+		INTERFACE, 
+		"GetUnitFileState", 
+		&error, 
+		&reply, 
+		"s",
+		unit
+		
+	);
+	if (r < 0) log_error(r, "GetUnitFileState call failed");
+	return reply;
 }
 
 int main(int argc, char **argv) {
@@ -29,23 +56,11 @@ int main(int argc, char **argv) {
 	r = sd_bus_open_user(&bus);
 	if (r < 0) return log_error(r, "Failed to acquire bus");
 
-	r = sd_bus_call_method(
-		  bus, 
-		  DESTINATION, 
-		  PATH, 
-		  INTERFACE, 
-		  MEMBER, 
-		  &error, 
-		  &reply, 
-		  "s", 
-		  "gitstatus.service"
-	);
-	if (r < 0) return log_error(r, MEMBER " call failed");
+	reply = get_unit_status(bus, error, reply, "gitstatus.service");
 
 	const char *ans;
 	r = sd_bus_message_read(reply, "s", &ans);
-	if (r < 0)
-	return log_error(r, "Failed to read reply");
+	if (r < 0) return log_error(r, "Failed to read reply");
 
 	printf("Unit path is \"%s\".\n", ans);
 
